3b525ea7eec3f08969c4c8b3048230d9ebdf0b48
Vincent <quatrevieux.vincent@gmail.com>
2016-02-12 13:53:48 +0100

    ADD General int. for game + ui + main()

---
+++ TheStudismeTheory/src/main/java/fr/thestudismetheory/TheStudismeTheory.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.thestudismetheory;
+
+/**
+ *
+ * @author q13000412
+ */
+public class TheStudismeTheory {
+    public static void main(String[] args) {
+        
+    }
+}

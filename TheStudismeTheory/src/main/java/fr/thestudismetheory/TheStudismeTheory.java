e5323f721e84ecc466751351d5c0ce178f66c67d
Nazim Lachter <lachtater@gmail.com>
2016-02-24 16:18:52 +0100

    Tests pour l'interface et ajout du MCD en image

--- TheStudismeTheory/src/main/java/fr/thestudismetheory/TheStudismeTheory.java
+++ TheStudismeTheory/src/main/java/fr/thestudismetheory/TheStudismeTheory.java
@@ -1,9 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package fr.thestudismetheory;
+
+import fr.thestudismetheory.ui.MainWindow;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.*;
 
 /**
  *
@@ -11,6 +13,33 @@ package fr.thestudismetheory;
  */
 public class TheStudismeTheory {
     public static void main(String[] args) {
+        JFrame frame = new JFrame("The Studisme Theory");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(800, 600));
+
+        //MainWindow mainWindow = new MainWindow();
+        //mainWindow.setOpaque(true);
+        //frame.setContentPane(mainWindow);
         
+        JPanel gameSection = new JPanel();
+        gameSection.setBorder(BorderFactory.createEtchedBorder());
+        
+        JPanel buttonsSection = new JPanel();
+        buttonsSection.setPreferredSize(new Dimension(200, 400));
+        buttonsSection.setBorder(BorderFactory.createEtchedBorder());
+        
+        JPanel statsSection = new JPanel();
+        statsSection.setPreferredSize(new Dimension(0, 100));
+        statsSection.setBorder(BorderFactory.createEtchedBorder());
+        
+        JPanel contentPane = new JPanel(new BorderLayout());
+        contentPane.add(gameSection, BorderLayout.CENTER);
+        contentPane.add(statsSection, BorderLayout.SOUTH);
+        contentPane.add(buttonsSection, BorderLayout.WEST);
+        
+        frame.setContentPane(contentPane);
+        
+        frame.pack();
+        frame.setVisible(true);
     }
 }
---
+++ TheStudismeTheory/src/main/java/fr/thestudismetheory/ui/MainWindow.java
@@ -0,0 +1,19 @@
+package fr.thestudismetheory.ui;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
+
+/**
+ * Created by Nazim on 24/02/2016.
+ */
+public class MainWindow extends JPanel {
+
+	private static final long serialVersionUID = 817081287995751183L;
+
+	public MainWindow() {
+
+    }
+}

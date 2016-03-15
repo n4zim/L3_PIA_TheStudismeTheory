2d40461cd6068ce8ab965b8fb10680516f5cea71
toopick <alexandrre-1313@hotmail.fr>
2016-03-15 21:00:14 +0100

    Début Fenetre School

--- src/main/java/fr/thestudismetheory/ui/TeacherWindow.java
+++ src/main/java/fr/thestudismetheory/ui/TeacherWindow.java
@@ -4,7 +4,7 @@ package fr.thestudismetheory.ui;
  * Fenetre permettant la gestion des professeurs d'une école
  * Created by Maeva on 11/03/2016.
  */
-public class TeacherWindow extends WindowConstants {
+public class TeacherWindow extends WindowConstants{
     public TeacherWindow(){
         super(UIConstants.TITLE_TEACHER);
     }
